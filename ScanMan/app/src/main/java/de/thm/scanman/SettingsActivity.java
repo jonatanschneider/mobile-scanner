998c9478d62972d0225f01932959fd0ce18e2ba4
Jonas Herzberger <jonas.herzberger@mni.thm.de>
2018-12-08 09:23:59 +0100

    created SettingsActivity

---
+++ ScanMan/app/src/main/java/de/thm/scanman/SettingsActivity.java
@@ -0,0 +1,31 @@
+package de.thm.scanman;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        // Show up button in action
+        ActionBar ac = getSupportActionBar();
+        if (ac != null) {
+            ac.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
