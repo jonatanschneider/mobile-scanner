d63431e144cd038dd58b85579cf358377bf2ba81
David Donges <david.donges@mni.thm.de>
2018-12-10 15:56:44 +0100

    Add LoginActivity
    Used the Android Studio login template and modified it to use firebase auth.

---
+++ ScanMan/app/src/main/java/de/thm/scanman/LoginActivity.java
@@ -0,0 +1,156 @@
+package de.thm.scanman;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * A login screen that offers login via email/password
+ */
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = LoginActivity.class.getSimpleName();
+
+    private FirebaseAuth auth;
+
+    private EditText emailView;
+    private EditText passwordView;
+    private View progressView;
+    private View loginFormView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // TODO: check if user is already logged in
+        auth = FirebaseAuth.getInstance();
+
+        emailView = findViewById(R.id.email);
+
+        passwordView = findViewById(R.id.password);
+        passwordView.setOnEditorActionListener((textView, id, keyEvent) -> {
+            if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+                attemptLogin();
+                return true;
+            }
+            return false;
+        });
+
+        Button mEmailSignInButton = findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(view -> attemptLogin());
+
+        loginFormView = findViewById(R.id.login_form);
+        progressView = findViewById(R.id.login_progress);
+    }
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+        // Reset errors
+        emailView.setError(null);
+        passwordView.setError(null);
+
+        // Store values at the time of the login attempt
+        String email = emailView.getText().toString();
+        String password = passwordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        if (!isPasswordValid(password)) {
+            passwordView.setError(getString(R.string.error_invalid_password));
+            focusView = passwordView;
+            cancel = true;
+        }
+
+        if (!isEmailValid(email)) {
+            emailView.setError(getString(R.string.error_field_required));
+            focusView = emailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error
+            focusView.requestFocus();
+        } else {
+            makeLogin(email, password);
+        }
+    }
+
+    /**
+     * Actually make the login request based on the credentials validated before and adjust the ui
+     * accordingly
+     * @param email
+     * @param password
+     */
+    private void makeLogin(String email, String password) {
+        // Show a progress spinner, and kick off a background task to
+        // perform the user login attempt
+        showProgress(true);
+
+        // Start firebase auth task
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, start main activity
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        // If sign in fails, show the form and an error message
+                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                        showProgress(false);
+                    }
+                });
+    }
+
+    private boolean isEmailValid(String email) {
+        return !TextUtils.isEmpty(email) && email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        return !TextUtils.isEmpty(password);
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    private void showProgress(final boolean show) {
+        int shortAnimationTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+        loginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        loginFormView.animate().setDuration(shortAnimationTime).alpha(
+                show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                loginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            }
+        });
+
+        progressView.setVisibility(show ? View.VISIBLE : View.GONE);
+        progressView.animate().setDuration(shortAnimationTime).alpha(
+                show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                progressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            }
+        });
+    }
+}
+
