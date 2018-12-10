e900c23424369a12a1fc478768ebd849ef9b95e2
Jonatan Schneider <jonatan.schneider@mni.thm.de>
2018-12-10 16:18:00 +0100

    Add UserLiveData

---
+++ ScanMan/app/src/main/java/de/thm/scanman/persistence/UserLiveData.java
@@ -0,0 +1,105 @@
+package de.thm.scanman.persistence;
+
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.thm.scanman.model.Document;
+import de.thm.scanman.model.User;
+
+public class UserLiveData extends LiveData<User> {
+    private Query query;
+    private String userId;
+    private LifecycleOwner owner;
+
+    private final UserListener userListener = new UserListener();
+
+    private class UserListener implements ValueEventListener {
+
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            Log.e("LiveData", "" + dataSnapshot);
+            User user = dataSnapshot.getValue(User.class);
+            user.setId(dataSnapshot.getKey());
+            //Auth; to check if user has rights to see documents
+
+            List<Document> createdDocuments = new ArrayList<>();
+            List<Document> sharedDocuments = new ArrayList<>();
+
+            List<String> sharedDocumentIds = new ArrayList<>();
+            DocumentDAO documentDAO = new DocumentDAO();
+
+            GenericTypeIndicator<List<String>> t = new GenericTypeIndicator<List<String>>() {
+            };
+
+            ScanmanLiveData liveDataCreatedDocuments = new ScanmanLiveData(FirebaseDatabase.createdDocsRef.child(userId));
+            liveDataCreatedDocuments.observe(owner, ds -> {
+                Log.e("LiveData2", "" + ds);
+                List<String> ids = null;
+                if (ds.getValue() != null) {
+                    ids = ds.getValue(t);
+
+                    ids.forEach(id -> {
+                        documentDAO.get(id, owner).observe(owner, doc -> {
+                            Log.e("LiveData Document", "" + doc);
+                            createdDocuments.add(doc);
+                        });
+                    });
+                }
+            });
+
+            ScanmanLiveData liveDataSharedDocuments = new ScanmanLiveData(FirebaseDatabase.sharedDocsRef.child(userId));
+            liveDataSharedDocuments.observe(owner, ds -> {
+                Log.e("LiveData2", "" + ds);
+                sharedDocumentIds.clear();
+                if (ds.getValue() != null) {
+                    sharedDocumentIds.addAll(ds.getValue(t));
+                    sharedDocumentIds.forEach(id -> {
+                        documentDAO.get(id, owner).observe(owner, temp -> {
+                            Log.e("LiveData Document", temp.toString());
+                            sharedDocuments.add(temp);
+                        });
+                    });
+                }
+            });
+
+            user.setCreatedDocuments(createdDocuments);
+            user.setSharedDocuments(sharedDocuments);
+
+            setValue(user);
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+            Log.e("UserLiveData", "DatabaseError: " + databaseError.getCode());
+        }
+    }
+
+    public UserLiveData(Query query, String userId, LifecycleOwner owner) {
+        this.query = query;
+        this.userId = userId;
+        this.owner = owner;
+    }
+
+    @Override
+    protected void onActive() {
+        query.addValueEventListener(userListener);
+        Log.d("LiveData", "User Live Data connected");
+    }
+
+    @Override
+    protected void onInactive() {
+        query.removeEventListener(userListener);
+        Log.d("LiveData", "User Live Data disconnected");
+    }
+}
