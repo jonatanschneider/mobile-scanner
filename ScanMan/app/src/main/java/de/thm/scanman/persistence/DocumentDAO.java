09d9235da5c97a8dd0557bd86ffe67638c84fa25
Jonatan Schneider <jonatan.schneider@mni.thm.de>
2018-12-10 16:17:24 +0100

    Add DocumentDAO

---
+++ ScanMan/app/src/main/java/de/thm/scanman/persistence/DocumentDAO.java
@@ -0,0 +1,31 @@
+package de.thm.scanman.persistence;
+
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.LiveData;
+
+import com.google.firebase.database.DatabaseReference;
+
+import de.thm.scanman.model.Document;
+
+public class DocumentDAO {
+
+   public void add(Document document) {
+        DatabaseReference reference = FirebaseDatabase.documentRef.push();
+        document.setId(reference.getKey());
+        reference.setValue(document);
+    }
+
+    public LiveData<Document> get(String docId, LifecycleOwner owner) {
+       return new DocumentLiveData(FirebaseDatabase.documentRef.child(docId), owner);
+    }
+
+    public void update(Document document) {
+        FirebaseDatabase.documentRef.child(document.getId()).setValue(document);
+
+    }
+
+    public void remove(String id) {
+        FirebaseDatabase.documentRef.child(id).removeValue();
+    }
+
+}
