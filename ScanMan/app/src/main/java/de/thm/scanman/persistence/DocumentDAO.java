de9b0539870a6fe6dba7fb3fd2f5c026f37ba0a6
Jonatan Schneider <jonatan.schneider@mni.thm.de>
2018-12-12 14:11:51 +0100

    Add method to addCreatedDocuments

--- ScanMan/app/src/main/java/de/thm/scanman/persistence/DocumentDAO.java
+++ ScanMan/app/src/main/java/de/thm/scanman/persistence/DocumentDAO.java
@@ -5,19 +5,31 @@ import android.arch.lifecycle.LiveData;
 
 import com.google.firebase.database.DatabaseReference;
 
+import java.util.List;
+
 import de.thm.scanman.model.Document;
+import de.thm.scanman.model.User;
 import de.thm.scanman.persistence.liveData.DocumentLiveData;
 
 public class DocumentDAO {
 
-   public void add(Document document) {
+    public void add(Document document) {
         DatabaseReference reference = FirebaseDatabase.documentRef.push();
         document.setId(reference.getKey());
         reference.setValue(document);
     }
 
-    public LiveData<Document> get(String docId, LifecycleOwner owner) {
-       return new DocumentLiveData(FirebaseDatabase.documentRef.child(docId), owner);
+    public void addCreatedDocuments(User user, List<Document> documentList) {
+        documentList.forEach(document -> {
+            DatabaseReference reference = FirebaseDatabase.createdDocsRef.child(user.getId()).push();
+            document.setId(reference.getKey());
+            reference.setValue(document);
+            FirebaseDatabase.documentRef.child(document.getId()).setValue(document);
+        });
+    }
+
+    public LiveData<List<Document>> get(DatabaseReference reference, LifecycleOwner owner) {
+        return new DocumentLiveData(reference, owner);
     }
 
     public void update(Document document) {
