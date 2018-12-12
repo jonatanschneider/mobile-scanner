09312c427dec04499e536a4a3bd7d8227bd0651c
Jonatan Schneider <jonatan.schneider@mni.thm.de>
2018-12-12 14:12:42 +0100

    Extend add to add createdDocuments into database

--- ScanMan/app/src/main/java/de/thm/scanman/persistence/UserDAO.java
+++ ScanMan/app/src/main/java/de/thm/scanman/persistence/UserDAO.java
@@ -5,15 +5,25 @@ import android.arch.lifecycle.LiveData;
 
 import com.google.firebase.database.DatabaseReference;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import de.thm.scanman.model.Document;
 import de.thm.scanman.model.User;
 import de.thm.scanman.persistence.liveData.UserLiveData;
 
 public class UserDAO {
 
     public void add(User user) {
+        List<Document> createdDocuments = user.getCreatedDocuments();
+        List<Document> sharedDocuments = user.getSharedDocuments();
+        user.setCreatedDocuments(new ArrayList<>());
+        user.setSharedDocuments(new ArrayList<>());
         DatabaseReference reference = FirebaseDatabase.usersRef.push();
         user.setId(reference.getKey());
         reference.setValue(user);
+        DocumentDAO documentDAO = new DocumentDAO();
+        documentDAO.addCreatedDocuments(user, createdDocuments);
     }
 
     public LiveData<User> get(String userId, LifecycleOwner owner) {
