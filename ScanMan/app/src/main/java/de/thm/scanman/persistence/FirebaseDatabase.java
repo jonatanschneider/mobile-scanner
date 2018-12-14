bffe1289f3c9797c0f9e48c687604120442da4ba
Jonatan Schneider <jonatan.schneider@mni.thm.de>
2018-12-14 08:59:44 +0100

    Make DAO references static

--- ScanMan/app/src/main/java/de/thm/scanman/persistence/FirebaseDatabase.java
+++ ScanMan/app/src/main/java/de/thm/scanman/persistence/FirebaseDatabase.java
@@ -3,17 +3,11 @@ package de.thm.scanman.persistence;
 import com.google.firebase.database.DatabaseReference;
 
 public class FirebaseDatabase {
-    public UserDAO userDAO;
-    public DocumentDAO documentDAO;
+    public static final UserDAO userDAO = new UserDAO();
+    public static final DocumentDAO documentDAO = new DocumentDAO();
     public static final DatabaseReference rootRef = com.google.firebase.database.FirebaseDatabase.getInstance().getReference();
     public static final DatabaseReference usersRef = rootRef.child("z_users");
     public static final DatabaseReference documentRef = rootRef.child("z_documents");
     public static final DatabaseReference createdDocsRef = rootRef.child("z_createdDocuments");
     public static final DatabaseReference sharedDocsRef = rootRef.child("z_sharedDocuments");
-
-    public FirebaseDatabase getDb() {
-        userDAO = new UserDAO();
-        documentDAO = new DocumentDAO();
-        return this;
-    }
 }
