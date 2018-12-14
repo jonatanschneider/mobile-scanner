8b47e9856be796cb44c61f02206a1a07f95348b1
Jonatan Schneider <jonatan.schneider@mni.thm.de>
2018-12-14 10:41:05 +0100

    Remove temporary name prefix

--- ScanMan/app/src/main/java/de/thm/scanman/persistence/FirebaseDatabase.java
+++ ScanMan/app/src/main/java/de/thm/scanman/persistence/FirebaseDatabase.java
@@ -6,7 +6,7 @@ public class FirebaseDatabase {
     public static final UserDAO userDAO = new UserDAO();
     public static final DocumentDAO documentDAO = new DocumentDAO();
     public static final DatabaseReference rootRef = com.google.firebase.database.FirebaseDatabase.getInstance().getReference();
-    public static final DatabaseReference usersRef = rootRef.child("z_users");
-    public static final DatabaseReference createdDocsRef = rootRef.child("z_createdDocuments");
-    public static final DatabaseReference sharedDocsRef = rootRef.child("z_sharedDocuments");
+    public static final DatabaseReference usersRef = rootRef.child("users");
+    public static final DatabaseReference createdDocsRef = rootRef.child("createdDocuments");
+    public static final DatabaseReference sharedDocsRef = rootRef.child("sharedDocuments");
 }
