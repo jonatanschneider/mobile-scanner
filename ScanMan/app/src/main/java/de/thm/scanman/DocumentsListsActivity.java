a2ff57228309e0805948dabde9807eae601c32d0
Tim Rösser <tim.roesser@mni.thm.de>
2018-12-21 18:46:51 +0100

    create working and clear version of tab bar

---
+++ ScanMan/app/src/main/java/de/thm/scanman/DocumentsListsActivity.java
@@ -0,0 +1,129 @@
+package de.thm.scanman;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class DocumentsListsActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
+
+    private TabLayout tabBar;
+    private ViewPager viewPager;
+    private FloatingActionButton addFab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_documents_lists);
+
+        getSupportActionBar().setElevation(0f);
+
+        tabBar = findViewById(R.id.tbl_main_content);
+        viewPager = findViewById(R.id.viewpager);
+        setUpPagerAndTabs();
+
+        addFab = findViewById(R.id.add_fab);
+        /*addFab.setOnClickListener(
+        // implement call for new intent here
+        );*/
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu
+        // This adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.documents_lists, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_stats:
+                // implement call for statistics here
+                //new StatsTask(this).execute(documents);
+                return true;
+            case R.id.action_settings:
+                // implement call for settings here
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // setup and style tab bar
+    private void setUpPagerAndTabs(){
+        tabBar.setTabTextColors(ContextCompat.getColor(this, android.R.color.black),
+                ContextCompat.getColor(this, R.color.colorAccent));
+        tabBar.setBackgroundColor(ContextCompat.getColor(this, R.color.colorTab));
+
+        viewPager.setAdapter(new TabAdapter(getSupportFragmentManager()));
+
+        tabBar.addOnTabSelectedListener(this);
+        tabBar.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
+    }
+
+    // TODO: Sollte man hier lieber FragmentPagerAdapter verwenden, weil es nur drei Fragments sind?
+    public class TabAdapter extends FragmentStatePagerAdapter {
+
+        private final String[] pageTitles = {
+                getApplicationContext().getString(R.string.all_documents),
+                getApplicationContext().getString(R.string.my_documents),
+                getApplicationContext().getString(R.string.shared_documents)
+        };
+
+        public TabAdapter(FragmentManager fm){
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return pageTitles.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            /*
+            Fragment fragment = new ViewPagerItemFragment();
+            Record r = records.get(position);
+            Bundle bundle = new Bundle();
+            bundle.putLong("id", r.id);
+            fragment.setArguments(bundle);
+            return fragment;
+            */
+            return ViewPagerItemFragment.getInstance(pageTitles[position]);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position){
+            return pageTitles[position];
+        }
+    }
+
+}
---
+++ ScanMan/app/src/main/java/de/thm/scanman/ViewPagerItemFragment.java
@@ -0,0 +1,68 @@
+package de.thm.scanman;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ViewPagerItemFragment extends Fragment {
+    private static final String PAGE_TITLE = "PAGE_TITLE";
+
+    private String pageTitle;
+
+    //private ListView documentsListView;
+
+    public ViewPagerItemFragment(){}
+
+    public static ViewPagerItemFragment getInstance(String pageTitle){
+        ViewPagerItemFragment fragment = new ViewPagerItemFragment();
+        Bundle args = new Bundle();
+        args.putString(PAGE_TITLE, pageTitle);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            this.pageTitle = getArguments().getString(PAGE_TITLE);
+        } else {
+            Log.d("TAG", "Error: no arguments!");
+        }
+
+        //documentsListView = findViewById(R.id.documents_list);
+        //documentsListView.setEmptyView(findViewById(R.id.documents_list_empty));
+
+        /*
+        if(tab.getText().toString().equals(getString(R.string.all_documents))) {
+            Fragment fragment = new ViewPagerItemFragment();
+            //Bundle bundle = new Bundle();
+            //bundle.putString("id", checkedRecord.getId());
+            //fragment.setArguments(bundle);
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment, "details")
+                    .commit();
+        }
+        */
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_view_pager_item, container, false);
+
+        //binding = FragmentRecordFormBinding.bind(view);
+        //binding.save.setOnClickListener(this);
+        //setHasOptionsMenu(true);
+
+        TextView content = ((TextView)view.findViewById(R.id.lbl_pager_item_content));
+        content.setText(pageTitle);
+
+        return view;
+    }
+
+}
