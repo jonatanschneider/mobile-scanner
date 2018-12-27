440e6eef1fcdd31a5fbf35cce1cb441ba7bf2068
Jonatan Schneider <jonatan.schneider@mni.thm.de>
2018-12-27 16:54:58 +0100

    Add updateOnCreated and updateOnShared Document functions

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,8 +1,40 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+/*export const onAddSharedDocument = functions.database.ref('/sharedDocuments/{userId}/{docId}').onCreate((snapshot, context) => {
+    const userId = context.params.userId
+    const docId = context.params.docId
+
+    const document = snapshot.val()
+
+    document.name = "Functions work!"
+    return snapshot.ref.update(document);
+})*/
+
+export const updateOnCreatedDocuments = functions.database.ref('/createdDocuments/{userId}/{docId}').onUpdate((change, context) => {
+    const document = change.before.val()
+    const docId = context.params.docId
+    const updates = []
+
+    document.userIds.forEach(userId => {
+        updates.push(change.before.ref.root.child(`sharedDocuments/${userId}/${docId}`).update(change.after.val()))
+    });
+
+    updates.push(change.before.ref.update(change.after.val()))
+    return Promise.all(updates)
+})
+
+export const updateOnSharedDocument = functions.database.ref('/sharedDocuments/{userId}/{docId}').onUpdate((change, context) => {
+    const document = change.before.val()
+    const docId = context.params.docId
+    const ownerId = document.ownerId
+    const updates = []
+
+    document.userIds.forEach(userId => {
+        updates.push(change.before.ref.root.child(`sharedDocuments/${userId}/${docId}`).update(change.after.val()))
+    });
+
+    updates.push(change.before.ref.root.child(`createdDocuments/${ownerId}/${docId}`).update(change.after.val()))
+
+    return Promise.all(updates)
+})
+
