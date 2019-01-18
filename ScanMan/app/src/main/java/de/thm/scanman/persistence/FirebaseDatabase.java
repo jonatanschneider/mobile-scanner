5130fa57c6d82a9af8690d054eaf61d6b2e7a53f
Jonatan Schneider <jonatan.schneider@mni.thm.de>
2019-01-18 16:58:00 +0100

    Add StorageReference

--- ScanMan/app/src/main/java/de/thm/scanman/persistence/FirebaseDatabase.java
+++ ScanMan/app/src/main/java/de/thm/scanman/persistence/FirebaseDatabase.java
@@ -1,6 +1,8 @@
 package de.thm.scanman.persistence;
 
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import de.thm.scanman.model.Document;
 
@@ -11,6 +13,8 @@ public class FirebaseDatabase {
     public static final DatabaseReference usersRef = rootRef.child("users");
     public static final DatabaseReference createdDocsRef = rootRef.child("createdDocuments");
     public static final DatabaseReference sharedDocsRef = rootRef.child("sharedDocuments");
+
+    public static final StorageReference documentStorage = FirebaseStorage.getInstance().getReference().child("documents");
 
     public static DatabaseReference getCreatedDocumentsReference(Document document) {
         return createdDocsRef.child(document.getOwnerId()).child(document.getId());
