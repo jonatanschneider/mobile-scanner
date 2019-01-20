c2273b38e8d1b5479731a44cdb660e068b502339
Jonatan Schneider <jonatan.schneider@mni.thm.de>
2019-01-20 16:26:09 +0100

    Load Document Images into ImageAdapter

--- ScanMan/app/src/main/java/de/thm/scanman/util/ImageAdapter.java
+++ ScanMan/app/src/main/java/de/thm/scanman/util/ImageAdapter.java
@@ -1,6 +1,7 @@
 package de.thm.scanman.util;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,7 +9,21 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import de.thm.scanman.R;
+import de.thm.scanman.persistence.FirebaseDatabase;
+import de.thm.scanman.persistence.GlideApp;
 
 /**
  * This class is used to display pictures in an Adapter
@@ -17,7 +32,7 @@ import java.util.List;
 public class ImageAdapter extends BaseAdapter {
     private Context mContext;
     private List<Uri> imagesList;
-    
+
     public ImageAdapter(Context c, List<Uri> imagesList) {
         mContext = c;
         this.imagesList = imagesList;
@@ -53,11 +68,19 @@ public class ImageAdapter extends BaseAdapter {
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
             imageView.setPadding(8, 8, 8, 8);
         }
-        else
-        {
+        else {
             imageView = (ImageView) convertView;
         }
-        imageView.setImageURI(imagesList.get(position));
+
+        setImage(imageView, position);
         return imageView;
+    }
+
+    private void setImage(ImageView view, int position) {
+        Uri uri = imagesList.get(position);
+        GlideApp.with(mContext)
+                .load(FirebaseDatabase.toStorageReference(uri))
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(view);
     }
 }
