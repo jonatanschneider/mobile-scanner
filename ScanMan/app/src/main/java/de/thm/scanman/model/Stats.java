47f9a914ea99566c7f66017338bce040d6661e9c
Jonatan Schneider <jonatan.schneider@mni.thm.de>
2019-02-12 17:23:21 +0100

    Add documentsSharedWithOthersFileSize

--- ScanMan/app/src/main/java/de/thm/scanman/model/Stats.java
+++ ScanMan/app/src/main/java/de/thm/scanman/model/Stats.java
@@ -2,11 +2,11 @@ package de.thm.scanman.model;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Stats {
 
     private User user;
-
 
     public Stats(User user) {
         this.user = user;
@@ -18,6 +18,14 @@ public class Stats {
 
     public long sharedDocumentsFileSize() {
         return documentsFileSize(user.getSharedDocuments());
+    }
+
+    public long documentsSharedWithOthersFileSize() {
+        List<Document> documents = user.getCreatedDocuments().stream()
+                .filter(doc -> doc.getUserIds() != null)
+                .filter(doc -> doc.getUserIds().size() > 0)
+                .collect(Collectors.toList());
+        return documentsFileSize(documents);
     }
 
     private long documentsFileSize(List<Document> list) {
