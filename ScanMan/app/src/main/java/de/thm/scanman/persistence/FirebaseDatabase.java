0ab53b949a048709f63ea428bcd766a81c728299
Jonatan Schneider <jonatan.schneider@mni.thm.de>
2019-03-11 17:16:51 +0100

    Add documentation to FirebaseDatabase

--- ScanMan/app/src/main/java/de/thm/scanman/persistence/FirebaseDatabase.java
+++ ScanMan/app/src/main/java/de/thm/scanman/persistence/FirebaseDatabase.java
@@ -8,6 +8,9 @@ import com.google.firebase.storage.StorageReference;
 
 import de.thm.scanman.model.Document;
 
+/**
+ * Contains static reference and methods to simplify work with Firebase references
+ */
 public class FirebaseDatabase {
     public static final UserDAO userDAO = new UserDAO();
     public static final DocumentDAO documentDAO = new DocumentDAO();
@@ -22,14 +25,30 @@ public class FirebaseDatabase {
     public static final int CREATED_DOCUMENT = 1;
     public static final int SHARED_DOCUMENT = 2;
 
+    /**
+     * Get DatabaseReference for a given document
+     * @param document
+     * @return DatabaseReference to the createdDocuments node
+     */
     public static DatabaseReference getCreatedDocumentsReference(Document document) {
         return createdDocsRef.child(document.getOwnerId()).child(document.getId());
     }
 
+    /**
+     * Get DatabaseReference of a sharedDocument for a given user
+     * @param userId of an user who has access to the given document
+     * @param document
+     * @return DatabaseReference to the sharedDocuments node
+     */
     public static DatabaseReference getSharedDocumentsReference(String userId, Document document) {
         return sharedDocsRef.child(userId).child(document.getId());
     }
 
+    /**
+     * Convert image URI to StorageReference
+     * @param uri image uri
+     * @return StorageReference of image
+     */
     public static StorageReference toStorageReference(Uri uri) {
         StorageReference reference = FirebaseStorage.getInstance().getReference();
         for (String segment : uri.getPathSegments()) {
